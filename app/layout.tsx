45f75e50497b133127568710f09c344aa9747007
rubeon <luganopizza@gmail.com>
2026-04-07 21:12:46 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,16 @@ import '@/src/index.css';
 import { Metadata } from 'next';
 import { AuthProvider } from '@/src/contexts/AuthContext';
 
+
 export const metadata: Metadata = {
   title: '로보어드바이저·ETF·환율 정보 정리 | robo-advisor.kr',
   description: '로보어드바이저 수수료, ETF 투자 기초, 환율 계산, 나스닥 선물 보는 법 등 금융 정보를 쉽게 정리한 가이드 사이트.',
   alternates: {
     canonical: 'https://robo-advisor.kr',
   },
+  icons: {
+  icon: "https://robo-advisor.kr/thumbnails/robofavicon.ico",
+},
   openGraph: {
     title: '로보어드바이저·ETF·환율 정보 정리 | robo-advisor.kr',
     description: '로보어드바이저 수수료, ETF 투자 기초, 환율 계산, 나스닥 실시간 확인법 등 금융 정보를 쉽게 정리한 가이드 사이트.',
